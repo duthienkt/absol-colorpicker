3770c5cb05766c669a3b3bb1b2afe326f713ab46
Pham Quoc Du Thien <blueskythien2010@live.com>
2019-12-18 15:10:00 +0700

    - update package

--- webpack.config.js
+++ webpack.config.js
@@ -31,11 +31,15 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
-                options: { presets: [[ '@babel/preset-env', { modules: false }]] }
+                options: { presets: [['@babel/preset-env', { modules: false }]] }
             },
             {
-                test: /\.(tpl|txt|xml|rels|css)$/i,
+                test: /\.(tpl|txt|xml|rels)$/i,
                 use: 'raw-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             }
         ]
     },
