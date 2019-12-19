a545a23cbffbf545203d4155205ccf234e3a80ea
Pham Quoc Du Thien <blueskythien2010@live.com>
2019-12-19 13:41:45 +0700

    - undo rename package

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 import CPCore from "./components/CPCore";
-import ColorPickerStyle from "./style";
-import './components/ColorIconInput';
+import "./style";
+import './components/ColorPickerButton';
 import Color from "absol/src/Color/Color";
 
 
 
-var ColorIconInput = {
+var ColorPicker = {
     core: CPCore,
     _: CPCore._,
     $: CPCore.$,
     creator: CPCore.creator,
-    $style: CPCore._({
-        tag:'style',
-        props:{
-            innerHTML: ColorPickerStyle
-        }
-    }).addTo(document.head),
     Color:Color
 };
 
-export default ColorIconInput;
+export default ColorPicker;
