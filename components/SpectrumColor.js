56ac76373c03334da7c14d6966ad93cd1198c14e
duthienkt <blueskythien2010@live.com>
2020-04-20 15:39:43 +0700

    +SpectrumColor

---
+++ components/SpectrumColor.js
@@ -0,0 +1,22 @@
+import CPCore from "./CPCore";
+var _ = CPCore._;
+var $ = CPCore.$;
+
+function SpectrumColor() {
+
+}
+
+SpectrumColor.render = function () {
+    return _({
+        class:'as-spectrum-color',
+        child: {
+            class:'as-spectrum-color-sat',
+            child:'.as-spectrum-color-val'
+        }
+    });
+};
+
+
+CPCore.install('spectrumcolor', SpectrumColor);
+
+export default SpectrumColor;
--- style/index.js
+++ style/index.js
@@ -1,3 +1,5 @@
 import './colorpicker.css';
 import './colorpickerbutton.css';
 import './swatchestable.css';
+import './spectrumcolor.css';
+
